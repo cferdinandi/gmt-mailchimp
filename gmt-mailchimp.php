b749d878a751a62d16b04277ec70dcdbe173c755
cferdinandi <chris@gomakethings.com>
2017-04-25 10:53:38 -0400

    Upgrade path - Added version number storage to the DB - Added data converstion function for people on an older version

--- gmt-mailchimp.php
+++ gmt-mailchimp.php
@@ -11,10 +11,13 @@
  * License: MIT
  */
 
+// Define constants
+define( 'GMT_MAILCHIMP_VERSION', '2.0.0' );
 
 // Includes
 require_once( plugin_dir_path( __FILE__ ) . 'includes/wp-session-manager/wp-session-manager.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'includes/helpers.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'includes/updates.php' );
 
 // Options
 require_once( plugin_dir_path( __FILE__ ) . 'includes/options.php' );
@@ -27,7 +30,34 @@ require_once( plugin_dir_path( __FILE__ ) . 'includes/metabox.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'includes/mailchimp.php' );
 
 
-// Flush rewrite rules on activation and deactivation
+/**
+ * Check the plugin version and make updates if needed
+ */
+function gmt_mailchimp_check_version() {
+
+	// Get plugin data
+	$old_version = get_site_option( 'gmt_mailchimp_version' );
+
+	// If plugin was just updated, make DB updates
+	if ( empty( $old_version ) || version_compare( $old_version, '2.0.0', '<' ) ) {
+		// Convert old data format to new one
+		gmt_mailchimp_update_interest_groups_2_0_0();
+	}
+
+	// Update plugin to current version number
+	if ( empty( $old_version ) || version_compare( $old_version, GMT_MAILCHIMP_VERSION, '<' ) ) {
+		update_site_option( 'gmt_mailchimp_version', GMT_MAILCHIMP_VERSION );
+	}
+
+	update_site_option( 'gmt_mailchimp_version', GMT_MAILCHIMP_VERSION );
+
+}
+add_action( 'plugins_loaded', 'gmt_mailchimp_check_version' );
+
+
+/**
+ * Flush rewrite rules on activation and deactivation
+ */
 function gmt_mailchimp_flush_rewrites() {
 	mailchimp_add_custom_post_type();
 	flush_rewrite_rules();
